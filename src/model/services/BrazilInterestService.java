debe05ba38fd05137733b78fd5255578461933d9
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-09-13 09:41:48 -0300

    Fix: Add validation to ensure the number of months is not less than one

--- src/model/services/BrazilInterestService.java
+++ src/model/services/BrazilInterestService.java
@@ -1,5 +1,7 @@
 package model.services;
 
+import java.security.InvalidParameterException;
+
 public class BrazilInterestService {
     private Double interestRate;
 
@@ -16,6 +18,9 @@ public class BrazilInterestService {
     }
 
     public double payment(Double amount, int months){
+        if (months <1) {
+            throw new InvalidParameterException("Months must be greater than 0");
+        }
         return amount * Math.pow(1+interestRate/100, months);
     }
 
